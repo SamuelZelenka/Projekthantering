618c2019c4f419435d97d33a71114063fd585647
peanutpowah <samuel.zelenka@hotmail.com>
2018-12-20 15:47:02 +0100

    inspect cards + add cards

--- Projekthantering/Assets/Script/CardHand.cs
+++ Projekthantering/Assets/Script/CardHand.cs
@@ -7,33 +7,44 @@ public class CardHand : MonoBehaviour {
     [SerializeField] float xOffset;
     [SerializeField] float zOffset;
     [SerializeField] float rotationZOffset;
-
+    RaycastHit hit;
+    Ray ray;
     GameObject myDeck;
+    GameObject inspectedCard;
 
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             AddCardFromDeck();
         }
-        if (myCards.Count != 0)
-        {
-                rotationZOffset = -100 / (myCards.Count);
 
-            for (int i = 0; i < myCards.Count; i++)
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.CompareTag("PlayerOneCard"))
             {
-                myCards[i].GetComponent<SpriteRenderer>().sortingOrder = i;
-                myCards[i].transform.position = new Vector3(transform.position.x + xOffset * i, transform.position.y, transform.position.z + zOffset * i);
-                myCards[i].transform.localRotation = Quaternion.Euler(90, 0, rotationZOffset * i);
+                if(inspectedCard != hit.collider.gameObject)
+                {
+                    SortCards();
+                }
+                inspectedCard = hit.collider.gameObject;
+                InspectCard(hit.collider.gameObject);
             }
-            transform.rotation = Quaternion.Euler(0, (rotationZOffset / 2) * (myCards.Count - 1), 0);
+
         }
+        else
+        {
+            SortCards();
+        }
+
     }
 
 
@@ -50,13 +61,32 @@ public class CardHand : MonoBehaviour {
         {
             print("out of cards");
         }
-        
     }
 
     public void AddCardToHand(GameObject newCard)
     {
         newCard.transform.SetParent(transform);
         myCards.Add(newCard);
-        
+        SortCards();
+    }
+    public void SortCards()
+    {
+        if (myCards.Count != 0)
+        {
+            rotationZOffset = -100 / (myCards.Count);
+
+            for (int i = 0; i < myCards.Count; i++)
+            {
+                myCards[i].GetComponent<SpriteRenderer>().sortingOrder = i;
+                myCards[i].transform.position = new Vector3(transform.position.x + xOffset * i, transform.position.y + (0.1f * i), transform.position.z + zOffset * i);
+                myCards[i].transform.localRotation = Quaternion.Euler(90, 0, rotationZOffset * i);
+            }
+            transform.rotation = Quaternion.Euler(0, (rotationZOffset / 2) * (myCards.Count - 1), 0);
+        }
+    }
+    void InspectCard(GameObject Card)
+    {
+            Card.GetComponent<SpriteRenderer>().sortingOrder = (myCards.Count + 1);
+            Card.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
     }
 }
