a69b75854cef422c61e424f2372c528114c7b35b
peanutpowah <samuel.zelenka@hotmail.com>
2018-12-20 10:42:00 +0100

    pick cards from deck

--- Projekthantering/Assets/Script/CardHand.cs
+++ Projekthantering/Assets/Script/CardHand.cs
@@ -8,6 +8,8 @@ public class CardHand : MonoBehaviour {
     [SerializeField] float zOffset;
     [SerializeField] float rotationZOffset;
 
+    GameObject myDeck;
+
 
     // Use this for initialization
     void Start () {
@@ -16,17 +18,39 @@ public class CardHand : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        rotationZOffset = -100 / (myCards.Count - 1);
-        for (int i = 0; i < myCards.Count; i++)
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            myCards[i].GetComponent<SpriteRenderer>().sortingOrder = i;
-            myCards[i].transform.position = new Vector3(transform.position.x + xOffset * i,transform.position.y, transform.position.z + zOffset * i);
-            myCards[i].transform.localRotation = Quaternion.Euler(90 ,0, rotationZOffset * i);
-            
-
-
+            AddCardFromDeck();
         }
-        transform.rotation = Quaternion.Euler(0, (rotationZOffset / 2) * (myCards.Count - 1), 0);
+        if (myCards.Count != 0)
+        {
+                rotationZOffset = -100 / (myCards.Count);
+
+            for (int i = 0; i < myCards.Count; i++)
+            {
+                myCards[i].GetComponent<SpriteRenderer>().sortingOrder = i;
+                myCards[i].transform.position = new Vector3(transform.position.x + xOffset * i, transform.position.y, transform.position.z + zOffset * i);
+                myCards[i].transform.localRotation = Quaternion.Euler(90, 0, rotationZOffset * i);
+            }
+            transform.rotation = Quaternion.Euler(0, (rotationZOffset / 2) * (myCards.Count - 1), 0);
+        }
+    }
+
+
+    public void AddCardFromDeck()
+    {
+
+        myDeck = GameObject.Find("CardDeckPlayerOne");
+
+        if(myDeck.transform.childCount > 0)
+        {
+            AddCardToHand(myDeck.transform.GetChild(0).gameObject);
+        }
+        else
+        {
+            print("out of cards");
+        }
+        
     }
 
     public void AddCardToHand(GameObject newCard)
