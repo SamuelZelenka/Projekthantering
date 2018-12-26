534e113bb3c571b94b17857ec21e867369186813
Samuel Z <samuel.zelenka@hotmail.com>
2018-12-26 16:45:51 +0100

    fonts

---
+++ Projekthantering/Assets/Script/GameController/TurnScripts/TurnButton.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class TurnButton : MonoBehaviour
+{
+
+    float maxTurnTime;
+    [SerializeField] float turnTimer;
+    bool myTurn;
+    Animation rotateAnim;
+    GameObject myPlayer;
+    Text uiTimer;
+    GameObject gameController;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.Find("GameController");
+        myPlayer = transform.parent.gameObject;
+        maxTurnTime = 30;
+        turnTimer = maxTurnTime;
+        myTurn = true;
+        rotateAnim = GetComponent<Animation>();
+        uiTimer = GameObject.Find("UITimer").GetComponent<Text>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIUpdate();
+        if (turnTimer <= 0)
+        {
+            if(myTurn)
+            {
+                EndTurn(); //if my turn and timer is 0, force end turn.
+            }
+            else
+            {
+                turnTimer = 0; //if not my turn and timer is <= 0. keep timer at 0
+            }
+        }
+        else
+        {
+            turnTimer -= Time.deltaTime; // countdown turntimer
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftControl)) //!PLACEHOLDER! for other players turn !PLACEHOLDER!
+        {
+            ResetTurn();
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (myTurn)
+        {
+            if (Input.GetAxis("Mouse 1") == 1) //if button is clicked on
+            {
+                EndTurn();
+            }
+        }
+    }
+    void EndTurn()
+    {
+        rotateAnim.Play("EndTurn");
+        myTurn = false;
+        gameController.GetComponent<GameController>().newTurn();
+    }
+
+    void ResetTurn()
+    {
+        rotateAnim.Play("ResetButton");
+        turnTimer = maxTurnTime;
+        myTurn = true;
+
+        myPlayer.GetComponentInChildren<CardHand>().AddCardFromDeck();
+
+    }
+    void UIUpdate()
+    {
+        uiTimer.text = $"{(int)turnTimer}";
+        if(turnTimer < 10)
+        {
+            uiTimer.color = new Color(255, 0, 0);
+        }
+        else
+        {
+            uiTimer.color = new Color(255, 255, 255);
+        }
+    }
+
+}
---
+++ Projekthantering/Assets/Script/Player/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int health { get { return health;} set { health += value;} }
+    public int mana { get { return mana; } set { mana += value; } }
+    public int armor { get { return armor; } set { armor += value; } }
+
+    int maxHealth = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
