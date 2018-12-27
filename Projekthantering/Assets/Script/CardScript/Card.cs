60a8d59ce39d53af44f79157bd4c572eaa6ce4e7
Samuel Z <samuel.zelenka@hotmail.com>
2018-12-27 16:09:18 +0100

    Player character visuals

--- Projekthantering/Assets/Script/CardScript/Card.cs
+++ Projekthantering/Assets/Script/CardScript/Card.cs
@@ -14,24 +14,25 @@ public class Card : MonoBehaviour
     Text showHp, showAttack, showCardName, showCardText, showManaCost, showCardType;
     Image showFrame, showPortrait;
     
+    
     // Start is called before the first frame update
     void Start()
     {
-        showHp = GameObject.Find("DisplayHp").GetComponent<Text>();
+        showHp = GameObject.Find($"{gameObject.name}/Canvas/DisplayHp").GetComponent<Text>();
         showHp.text = "" + hp;
-        showAttack = GameObject.Find("DisplayAttack").GetComponent<Text>();
+        showAttack = GameObject.Find($"{gameObject.name}/Canvas/DisplayAttack").GetComponent<Text>();
         showAttack.text = "" + attack;
-        showCardName = GameObject.Find("DisplayCardName").GetComponent<Text>();
+        showCardName = GameObject.Find($"{gameObject.name}/Canvas/DisplayCardName").GetComponent<Text>();
         showCardName.text = cardName;
-        showCardText = GameObject.Find("DisplayCardText").GetComponent<Text>();
+        showCardText = GameObject.Find($"{gameObject.name}/Canvas/DisplayCardText").GetComponent<Text>();
         showCardText.text = cardText;
-        showManaCost = GameObject.Find("DisplayManaCost").GetComponent<Text>();
+        showManaCost = GameObject.Find($"{gameObject.name}/Canvas/DisplayManaCost").GetComponent<Text>();
         showManaCost.text = "" + manaCost;
-        showCardType = GameObject.Find("DisplayCardType").GetComponent<Text>();
+        showCardType = GameObject.Find($"{gameObject.name}/Canvas/DisplayCardType").GetComponent<Text>();
         showCardType.text = cardType;
-        showFrame = GameObject.Find("DisplayCardFrame").GetComponent<Image>();
+        showFrame = GameObject.Find($"{gameObject.name}/Canvas/DisplayCardFrame").GetComponent<Image>();
         showFrame.sprite = frame;
-        showFrame = GameObject.Find("DisplayPortrait").GetComponent<Image>();
+        showFrame = GameObject.Find($"{gameObject.name}/Canvas/DisplayPortrait").GetComponent<Image>();
         showFrame.sprite = portrait;
     }
 
