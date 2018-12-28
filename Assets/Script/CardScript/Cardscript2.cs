4dc0623d04de333682444742a46f0855e5c25f39
ChiiVik <NVMviktor@gmail.com>
2018-12-28 11:57:14 +0100

    drag n drop card

---
+++ Assets/Script/CardScript/Cardscript2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cardscript2 : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {/*
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.up, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.transform.position.z < this.gameObject.transform.position.z)
+            {
+                gameObject.transform.position = new Vector3(0.16f, 2.7f, 4.08f + 1.50f);
+            }
+
+            else if (hit.collider.transform.position.z > this.gameObject.transform.position.z)
+            {
+                gameObject.transform.position = new Vector3(0.16f, 2.7f, 4.08f - 1.50f);
+            }
+
+        }*/
+    }
+}
